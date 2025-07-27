da3dd7e89eaba80ee216879cdb0f36bb688ae755
hzswdef <ostelia.group@gmail.com>
2025-07-27 21:04:07 +0300

    #7qwKSd8K: Added partially done (WIP) PawsCheckbox component.

---
+++ src/components/PawsCheckbox/PawsCheckbox.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import styles from "./PawsCheckbox.module.css";
+
+export interface PawsCheckboxProps {
+	label: string;
+	checked?: boolean;
+}
+
+withDefaults(defineProps<PawsCheckboxProps>(), {
+	checked: false,
+});
+</script>
+
+<template>
+	<label :class="styles.checkbox">
+		<input type="checkbox" checked="checked" />
+		<span :class="styles.checkmark"></span>
+		<span>{{ label }}</span>
+	</label>
+</template>

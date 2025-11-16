3f03406f95ab6a19af36412734eb115fb803d0d2
lol im pitahaya <55537219+shsh-x@users.noreply.github.com>
2025-11-16 21:16:22 +0300

    fix: added imports for new elements; changed versioning

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import type { App } from "vue";
 
 import PawsButton from "@/components/PawsButton/PawsButton.vue";
 import PawsCard from "@/components/PawsCard/PawsCard.vue";
+import PawsCheckbox from "@/components/PawsCheckbox/PawsCheckbox.vue";
+import PawsColorPickerButton from "@/components/PawsColorPickerButton/PawsColorPickerButton.vue";
 import PawsDropdown from "@/components/PawsDropdown/PawsDropdown.vue";
+import PawsField from "@/components/PawsField/PawsField.vue";
 import PawsHeading from "@/components/PawsHeading/PawsHeading.vue";
 import PawsInput from "@/components/PawsInput/PawsInput.vue";
 import PawsMultiSwitch from "@/components/PawsMultiSwitch/PawsMultiSwitch.vue";
@@ -16,7 +19,10 @@ import PawsResizeButton from "@/components/PawsResizeButton/PawsResizeButton.vue
 const install = (app: App): void => {
 	app.component("PawsButton", PawsButton);
 	app.component("PawsCard", PawsCard);
+	app.component("PawsCheckbox", PawsCheckbox);
+	app.component("PawsColorPickerButton", PawsColorPickerButton);
 	app.component("PawsDropdown", PawsDropdown);
+	app.component("PawsField", PawsField);
 	app.component("PawsHeading", PawsHeading);
 	app.component("PawsInput", PawsInput);
 	app.component("PawsMultiSwitch", PawsMultiSwitch);
@@ -29,7 +35,10 @@ export default install;
 export {
 	PawsButton,
 	PawsCard,
+	PawsCheckbox,
+	PawsColorPickerButton,
 	PawsDropdown,
+	PawsField,
 	PawsHeading,
 	PawsInput,
 	PawsMultiSwitch,
